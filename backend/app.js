fc3d891358cdb58dc357c40b027ffd956180d383
darlwhizkid <darlingtonodom@gmail.com>
2025-02-18 07:23:58 +0100

    installed cors

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
+const cors = require('cors');
+
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use('/api/auth', authRoutes);
